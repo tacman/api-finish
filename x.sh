0acb7e9e013f95e1b713c9e820afc6304dcd3dcf
Tac Tacelosky <tacman@gmail.com>
2022-06-17 08:03:43 -0400

    update recipe

--- x.sh
+++ x.sh
@@ -1,1 +1,1 @@
-composer recipes:install --force symfony/$1 && git add . && git commit -m "update recipe"
+composer recipes:install --force symfony/$1 && git add . && git commit -m "update recipe" && composer recipes
